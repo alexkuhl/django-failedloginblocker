eed8f73fd631ff0482acae869eafc971bf3aac2c
Drew Volpe <accounts@dewdrops.net>
2011-10-07 14:04:57 -0400

    Remove requirement that a username is passed into auth function (this breaks django auth tests).

--- failedloginblocker/decorators.py
+++ failedloginblocker/decorators.py
@@ -16,10 +16,6 @@ def monitor_login( auth_func ):
     def decorate( *args, **kwargs ):
         """ Wrapper for Django authentication function """
         user = kwargs.get( 'username', '' )
-        if not user:
-            raise ValueError( 'username must be supplied by the \
-                authentication function for FailedLoginBlocker to operate' )
-                
         try:
             fa = FailedAttempt.objects.get( username=user )
             if fa.recent_failure( ):
